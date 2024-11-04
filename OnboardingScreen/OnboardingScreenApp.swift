2c7ef9fa74c1eb92a6c0ddcaac5a76c605f971d7
Batuhanirmali <batuqwert1@gmail.com>
2024-11-04 12:40:43 +0300

    Initial Commit

---
+++ OnboardingScreen/OnboardingScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingScreenApp.swift
+//  OnboardingScreen
+//
+//  Created by Talha Batuhan Irmalı on 4.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
